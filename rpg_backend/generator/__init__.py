b43490b9da079eaa84ce8fcf756310954587b874
ShehaoLi <shl145@ucsd.edu>
2026-03-03 19:32:58 -0800

    refactor(generator): move generate_pack orchestration into pipeline

--- rpg_backend/generator/__init__.py
+++ rpg_backend/generator/__init__.py
@@ -1,5 +1,7 @@
 """Story generation modules for deterministic v3 pack creation."""
 
-from rpg_backend.generator.service import GeneratorBuildError, GeneratorService
+from rpg_backend.generator.errors import GeneratorBuildError
+from rpg_backend.generator.result_builder import GeneratorBuildResult
+from rpg_backend.generator.service import GeneratorService
 
-__all__ = ["GeneratorService", "GeneratorBuildError"]
+__all__ = ["GeneratorService", "GeneratorBuildError", "GeneratorBuildResult"]
